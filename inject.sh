b9ca9a713321b483794c021e9d7cd79e09702494
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2023-03-28 16:02:27 +1000

    Update inject.sh

--- inject.sh
+++ inject.sh
@@ -1,76 +1,58 @@
 #!/bin/bash
 
-sudo useradd ken
-sudo useradd zangief
-sudo useradd sagat
-sudo useradd bison
-sudo useradd akuma
-sudo useradd ryu
-sudo useradd vega
-sudo useradd balrog
-sudo passwd -d vega
-sudo passwd -d akuma
-sudo usermod -aG sudo akuma
+#standard password change
 sudo echo -e "ubuntu\nubuntu" | passwd ubuntu
-sudo chown bison /etc/passwd
-sudo echo 'shadowlooisthebest' > /opt/listen.sh
-sudo mkdir /home/vega /home/ubuntu
-sudo mkdir /home/vega/Desktop /home/ubuntu/Desktop
-sudo echo 'thisisnotavirus' > /home/vega/Desktop/supercombo.mp4
-sudo echo 'auth sufficient pam_succeed_if.so user ingroup nopasswdlogin' > /etc/pam.d/gdm-password
-sudo groupadd nopasswdlogin
-sudo gpasswd --add vega nopasswdlogin
-sudo groupadd antagonistboss
-sudo usermod -aG antagonistboss balrog
-sudo usermod -aG antagonistboss sagat
-sudo usermod -aG antagonistboss bison
-sudo sed -i "/Banner/d" /etc/ssh/sshd_config
-sudo mkdir -p /home/sagat/Desktop/message/.dir4
-sudo mkdir -p /home/sagat/Desktop/message/dir3
-sudo mkdir -p /home/sagat/Desktop/message/dir2
-sudo mkdir -p /home/sagat/Desktop/message/dir1
 
-sudo echo 'AGEAbgBnAHIAeQAgAHMAYwBhAHI=' >  /home/sagat/Desktop/message/my\ message.txt
-sudo echo 'thaifoodisawesome' > /home/sagat/Desktop/message/.dir4/.hiddenmessage.txt
-sudo touch /home/sagat/Desktop/message/dir1/hiddenmessage.txt
-sudo touch /home/sagat/Desktop/message/dir2/hiddenmessage.txt
-sudo touch /home/sagat/Desktop/message/dir3/hiddenmessage.txt
-sudo touch /home/sagat/Desktop/hiddenmessage.txt
+#Add real users
+sudo useradd shyguy
+sudo useradd boo
+sudo useradd iggy
+sudo useradd kamek
+sudo useradd bulletbill
+sudo useradd lemmy
+sudo useradd morton
 
-sudo mkdir -p /home/vega/Desktop/filehere
-sudo mkdir -p /home/bison/Desktop/filehere/orhere/orthere/oranywhere
+#add sudo
+sudo usermod -aG sudo boo
 
+#Add unauthorized users
+sudo useradd Morton
+sudo useradd luigi
+sudo useradd yoshi
 
-sudo echo 'maybethis' > /home/bison/Desktop/filehere/possiblelink.txt
-sudo echo 'orthis' > /home/bison/Desktop/filehere//orhere/possiblelink.txt
-sudo echo 'orthat' > /home/bison/Desktop/filehere/orhere/orthere/possiblelink.txt
-sudo echo 'orthat' > /home/bison/Desktop/filehere/orhere/orthere/possiblelink.txt
-sudo echo 'orthat' > /home/bison/Desktop/filehere/orhere/orthere/oranywhere/possiblelink.txt
-sudo ln -s /home/bison/Desktop/filehere/orhere/orthere/possiblelink.txt /home/vega/Desktop/filehere/linked.txt
+#create group
+sudo usermod -aG koopakids iggy
+sudo usermod -aG koopakids lemmy
+sudo usermod -aG koopakids kamek
 
+#make directories
+sudo mkdir -p /home/bulletbill/desktop/banzaibill/.kaboom
+sudo mkdir -p /home/kamek/desktop/magikoopa
+sudo mkdir -p /home/morton/desertland/giantland/.koopacastle
+
+#create forensics questions
+
+#forensic1 - UID of user lemmy
+
+#forensic2 - decode message 
+sudo echo 'findprincesspeach' | tr a-z v-za-u > /home/bulletbill/desktop/banzaibill/communication.txt
+
+#forensic3 - find hidden number message among the messages. Number is in the same line as letter W
+sudo echo -e "1aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n2aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n3aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n4aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n5aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n6aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n7aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n8aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n9aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n10aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n11aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n12aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n13aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n14aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n15aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n16aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n17aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n18aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n19aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n20aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n21aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n22aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n23aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n24aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n25aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n26aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n27aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n28aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n29aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n30aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n31aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n32aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n33aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n34aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n35aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n36aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n37aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n38aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n39aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n40aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" > /home/kamek/desktop/magikoopa/message1.txt
+sudo cp /home/kamek/desktop/magikoopa/message1.txt /home/kamek/desktop/magikoopa/message2.txt /home/kamek/desktop/magikoopa/message3.txt /home/kamek/desktop/magikoopa/message5.txt /home/kamek/desktop/magikoopa/message6.txt /home/kamek/desktop/magikoopa/message7.txt
+sudo echo -e "1aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n2aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n3aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n4aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n5aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n6aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n7aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n8aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n9aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n10aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n11aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n12aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n13aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n14aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n15aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n16aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n17aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n18aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n19aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n20aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n21aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n22aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n23aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n24aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n25aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n26aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n27aaaaaaaaaaaFirefloweraaaaaaaa64aaaaaaaaaaaaaa\n28aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n29aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n30aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n31aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n32aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n33aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n34aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n35aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n36aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n37aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n38aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n39aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n40aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" > /home/kamek/desktop/magikoopa/message4.txt
+
+#Bad files
+
+sudo echo 'itsamemario' > /home/morton/desertland/video.mp4
+sudo echo 'builttobedestroyed' > /home/morton/desertland/giantland/.koopacastle/koopacastlelayout.xlsx
+sudo echo 'wariocycle' > /home/bulletbill/desktop/banzaibill/.kaboom/wariocycle.tiff
+sudo echo 'thisisabackdoor' > /home/morton/desertland/giantland/notabackdoor.js
+
+#applications
 sudo apt update 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq postfix < /dev/null > /dev/null
-sudo apt install gedit vim sqlite3 john rkhunter fatsort apache2 netcat mysql-server -y -qq
-sudo service apache2 stop
+sudo apt install gedit vim sqlite3 DNSenum Dirb mysql-server -y -qq
+sudo service sqlite3 stop
 
-sudo sed -i '/Port/d' /etc/ssh/sshd_config
-sudo sed -i '/PermitRoot/d' /etc/ssh/sshd_config
-sudo sed -i '/PermitUserEnvironment/d' /etc/ssh/sshd_config
-sudo sed -i '/PermitEmptyPasswords/d' /etc/ssh/sshd_config
-sudo sed -i '/Protocol/d' /etc/ssh/sshd_config
-sudo sed -i '/UsePAM/d' /etc/ssh/sshd_config
 
-sudo sed -i '$ a Port 22' /etc/ssh/sshd_config
-sudo sed -i '$ a PermitRootLogin yes' /etc/ssh/sshd_config
-sudo sed -i '$ a PermitUserEnvironment no' /etc/ssh/sshd_config
-sudo sed -i '$ a PermitEmptyPasswords yes' /etc/ssh/sshd_config
-sudo sed -i '$ a Protocol 1' /etc/ssh/sshd_config
-sudo sed -i '$ a UsePAM no' /etc/ssh/sshd_config
-
-sudo sed -i '/security/d' /etc/apt/sources.list
-sudo sed -i '/Update-Package-Lists/d' /etc/apt/apt.conf.d/10periodic
-sudo sed -i '$ a APT::Periodic::Update-Package-Lists "0";' /etc/apt/apt.conf.d/10periodic
- 
-sudo sysctl -w kernel.dmesg_restrict=0
-sudo sysctl -w kernel.ctrl-alt-del=1
-sudo chmod 777 /etc/shadow
