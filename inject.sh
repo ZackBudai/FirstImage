b77e3bf19bd0cfdb682bbee6ae5b536554563242
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2023-04-03 14:22:04 +1000

    Update inject.sh

--- inject.sh
+++ inject.sh
@@ -97,6 +97,7 @@ Vigenere Cipher, key is found in another folder with chown, base64key
 sudo echo 'cwftftkymttlnpgtpzythggyu' > /home/MazdaB787/desktop/24hour/lemans/whodrivesforthatlonganyway/
 #forensic 2 - decode text in file.
 sudo echo 'CLUTCH' | base64 > /home/PaganiZondaF/desktop/itsnotacar/.thekeyis
+sudo chown 1006 /home/PaganiZondaF/desktop/itsnotacar/.thekeyis
 
 
 #standard password change
