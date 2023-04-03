a57802adb6279b28bbd3df851d52833ce581b1e2
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2023-04-03 14:06:21 +1000

    Update inject.sh

--- inject.sh
+++ inject.sh
@@ -24,7 +24,6 @@ sudo useradd SaleenS7
 sudo useradd DMCDeLorean
 sudo useradd MaclarenF1
 sudo useradd AstonMartinVanquish
-sudo useradd MaclarenF1
 sudo useradd BugattiVeyron
 sudo useradd LamborghiniDiablo
 sudo useradd PaganiZondaF
@@ -59,72 +58,44 @@ sudo usermod -aG italiancraft FerrariItalia458
 #to score if removed from italiancraft: BugattiVeyron
 #to score if added to italiancraft: LamborghiniDiablo LamborghiniCountach MaseratiGranTurismo 
 
+# to score if group added: japanesecars
+# To score if added to group japanesecars: MazdaFurai ToyotaAE86 NissanGTRR35 MazdaB787 MitsuokaOrochi
+
+#To score if group created: germanworkmanship
+#To score if added to group germanworkmanship: MercedesAMGSLS Porsche911 AudiR8 ApolloEnraged
 
 
-To score: Create japanesecars
-To score if aadded
-MazdaFurai
-ToyotaAE86
-NissanGTRR35
-MazdaB787
-MitsuokaOrochi
-
-To score: Create germanworkmanship
-To score if added
-MercedesAMGSLS
-Porsche911
-AudiR8
-ApolloEnraged
+#make directories
+sudo mkdir -p /home/LamborghiniDiablo/desktop/theoriginal/supercar/
+sudo mkdir -p /home/PaganiZondaF/desktop/itsnotacar/itsarocket/
+sudo mkdir -p /home/MazdaB787/desktop/24hour/lemans/whodrivesforthatlonganyway/
+sudo mkdir -p /home/SaleenS7/desktop/american/supercar/butdriveontheleftwhy/
+sudo mkdir -p /home/DMCDeLorean/desktop/backtothefuture/timemachine/
 
 
 
-Forensic 1
-Find UID of LamborghiniDiablo
+#Illegal videos
+sudo echo 'isthisarealcarornot' > /home/PaganiZondaF/desktop/itsnotacar/itsarocket/.redbullx1concept.mp4
+sudo echo 'quartermilestraightbutthebugattiturnslikeatank' > /home/LamborghiniDiablo/desktop/theoriginal/supercar/bugattiporschedragrace.mov
+
+#Legalpictures
+sudo echo 'everyonesayit.1.21jigawatts' > /home/DMCDeLorean/desktop/backtothefuture/timemachine/deloreantimemachine.png
+sudo echo 'cambreangleaffectshowsharpthecarcanturn' > /home/SaleenS7/desktop/american/supercar/butdriveontheleftwhy/chevyzr1cambreangle.jfif
+
+
+#To score if Firewall up
+
+#create forensics questions
+#Forensic 1
+#Find UID of LamborghiniDiablo (1008)
 
 Forensic 2
 Decode 'LeMansWinners' text
 Vigenere Cipher, key is found in another folder with chown, base64key
 
-Illegal videos
-redbullx1concept.mp4
-bugattiporschedragrace.mov
-
-Legalpictures
-deloreantimemachine.png
-chevyzr1cambreangle.jfif
-
-Firewall up
-
-
-
-
-
-
-
-#make directories
-sudo mkdir -p /home/quokka/desktop/aussie/aussie/aussie/
-sudo mkdir -p /home/wallaby/desktop/spirit/ofaustralia/
-sudo mkdir -p /home/echidna/desktop/waltzing/mathilda/
-sudo mkdir -p /home/emu/desktop/soccer/notfootball/why/
-sudo mkdir -p /home/kookabura/desktop/whydopeoplelike/vegemite/
-
-
-#create forensics questions
-
-#forensic1 - owner of some particular file
-sudo echo 'timtamsarebetter' > /home/kookabura/desktop/whydopeoplelike/vegemite/secret.txt
-sudo chown opossum /home/kookabura/desktop/whydopeoplelike/vegemite/secret.txt
-
 #forensic2 - decode message 
 sudo echo 'happiestanimalintheworld' | tr a-z k-za-l | base64 > /home/quokka/desktop/aussie/aussie/aussie/decode.txt
 
-#Bad files 
-sudo echo 'itscalledmaccasnotmcdonalds' > /home/wallaby/desktop/spirit/ofaustralia/boomerang.mp4
-sudo echo 'this isnt actually a video file its a text file but i turned it into a video how' > /home/emu/desktop/soccer/notfootball/why/timcahillbestgoals.mp4
-
-#allowed files
-sudo echo 'itsanawesomesong' > /home/wallaby/desktop/spirit/ofaustralia/istillcallaustraliahome.jpg
-sudo echo 'buyasausageatahardwarestore' > /home/quokka/desktop/aussie/aussie/aussie/sausagesizzle.tiff
 
 #standard password change
 sudo echo -e "ubuntu\nubuntu" | passwd ubuntu
